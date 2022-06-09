7c695d06e6b30171beeacd1b309164fc4e0edff7
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2022-06-09 14:27:46 +0900

    feat: add tls external service

--- circuit-beaker-sidecar/app/main.go
+++ circuit-beaker-sidecar/app/main.go
@@ -13,6 +13,7 @@ func main() {
 	}
 	http.HandleFunc("/", h.alive)
 	http.HandleFunc("/test", h.external)
+	http.HandleFunc("/httpbin", h.httpbin)
 	http.ListenAndServe(":8001", nil)
 }
 
@@ -41,5 +42,25 @@ func (h *handler) external(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "external service returns 504")
 	default:
 		fmt.Fprintf(w, "Succeed in calling external api")
+	}
+}
+
+func (h *handler) httpbin(w http.ResponseWriter, r *http.Request) {
+	cli := http.DefaultClient
+	resp, err := cli.Get("http://sidecar:8083/status/200")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	switch resp.StatusCode {
+	case 503:
+		w.WriteHeader(http.StatusServiceUnavailable)
+		fmt.Fprintf(w, "httpbin returns 503")
+	case 504:
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "httpbin returns 504")
+	default:
+		fmt.Fprintf(w, "Succeed in calling httpbin")
 	}
 }
--- circuit-beaker-sidecar/k6/script.js
+++ circuit-beaker-sidecar/k6/script.js
@@ -2,12 +2,13 @@ import http from "k6/http";
 import { check } from "k6";
 
 export const options = {
-  vus: 30,
-  duration: "1m",
+  vus: 20,
+  duration: "30s",
 };
 export default function () {
-  const res = http.get("http://localhost:8080/test");
+  const res = http.get("http://localhost:8080/httpbin");
   check(res, {
     "is status 200": (r) => r.status === 200,
+    "is status 503": (r) => r.status === 503,
   });
 }
