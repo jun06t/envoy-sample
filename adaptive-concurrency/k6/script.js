4f1c2e840906316d4922ee908b3ece15414632bb
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2022-06-10 12:48:21 +0900

    fix: load test scenario

--- adaptive-concurrency/k6/script.js
+++ adaptive-concurrency/k6/script.js
@@ -2,8 +2,17 @@ import http from "k6/http";
 import { check } from "k6";
 
 export const options = {
-  vus: 10,
-  duration: "1m",
+  scenarios: {
+    ramping_up_scenario: {
+      executor: "ramping-vus",
+      startVUs: 10,
+      stages: [
+        { duration: "1m", target: 10 },
+        { duration: "30s", target: 60 },
+        { duration: "1m", target: 10 },
+      ],
+    },
+  },
 };
 export default function () {
   const res = http.get("http://localhost:8080/cpu");
